018b108e91c5a0af33f9c3f553c91aef7331fbc8
Serhii Haber <haberserhii@icloud.com>
2023-06-20 15:42:55 +0300

    update

---
+++ Module 2./Autocheck #14.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "plum", "pear", "orange", "banana"];
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, -1);
+const lastThreeEls = fruits.slice(-3);
+
+console.log(firstTwoEls);
+console.log(nonExtremeEls);
+console.log(lastThreeEls);
