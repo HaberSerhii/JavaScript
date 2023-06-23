51261bc76c961e236c6e027c4f4f561b3eea8e8a
Serhii Haber <haberserhii@icloud.com>
2023-06-23 21:02:01 +0300

    Update Autocheck #21.js

---
+++ Module 2./Autocheck #21.js
@@ -0,0 +1,12 @@
+function calculateTotalPrice(order) {
+  let total = 0;
+  for (i = 0; i < order.length; i += 1) {
+    const item = order[i];
+    total += item;
+  }
+  return total;
+}
+
+console.log(calculateTotalPrice[(12, 85, 37, 4)]);
+console.log(calculateTotalPrice[(164, 48, 291)]);
+console.log(calculateTotalPrice[(412, 371, 94, 63, 176)]);
