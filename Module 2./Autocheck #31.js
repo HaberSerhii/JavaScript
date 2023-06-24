fed5c27e1187e4b66a69c7c540c76c4d142aa5cb
Serhii Haber <haberserhii@icloud.com>
2023-06-24 11:59:29 +0300

    Update Autocheck #31.js

---
+++ Module 2./Autocheck #31.js
@@ -0,0 +1,13 @@
+function findNumber(start, end, divisor) {
+  let number;
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      return (number = i);
+    }
+  }
+}
+console.log(findNumber(2, 6, 5));
+console.log(findNumber(8, 17, 3));
+console.log(findNumber(6, 9, 4));
+console.log(findNumber(16, 35, 7));
