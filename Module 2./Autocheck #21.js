aae9863628d7df0bc915ee26e4f38e8f3c8bfa04
Serhii Haber <haberserhii@icloud.com>
2023-06-24 09:10:48 +0300

    update

--- Module 2./Autocheck #21.js
+++ Module 2./Autocheck #21.js
@@ -7,6 +7,6 @@ function calculateTotalPrice(order) {
   return total;
 }
 
-console.log(calculateTotalPrice[(12, 85, 37, 4)]);
-console.log(calculateTotalPrice[(164, 48, 291)]);
-console.log(calculateTotalPrice[(412, 371, 94, 63, 176)]);
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
---
+++ Module 2./Autocheck #22.js
@@ -0,0 +1,17 @@
+function findLongestWord(string) {
+  let words = string.split(" ");
+  let longestWord = "";
+
+  for (i = 0; i < words.length; i += 1) {
+    const word = words[i];
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWord("Google do a roll"));
+console.log(findLongestWord("May the force be with you"));
