09c04e522d8bb0add5c2a653e245071d8120b575
Serhii Haber <haberserhii@icloud.com>
2023-06-28 16:39:33 +0300

    Update Autocheck #11.js

---
+++ Module 3./Autocheck #11.js
@@ -0,0 +1,18 @@
+const keys = [];
+const values = [];
+const advert = {
+  service: "apt",
+};
+const apartment = Object.create(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  if (apartment.hasOwnProperty(key)) {
+    keys.push(key);
+    values.push(apartment[key]);
+  }
+}
+
+console.log(apartment);
