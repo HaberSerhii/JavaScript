41c25197b2a8fd2e5c5d8a5478a37c127a09cc85
Serhii Haber <haberserhii@icloud.com>
2023-06-28 13:06:33 +0300

    Update

---
+++ Module 3./Autocheck #6.js
@@ -0,0 +1,19 @@
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = "Henry Sibola";
+apartment.tags.push("trusted");
+
+console.log(apartment);
---
+++ Module 3./Autocheck #8.js
@@ -0,0 +1,13 @@
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  name,
+  price,
+  image,
+  tags,
+};
+
+console.log(product);
