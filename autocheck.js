fa3a1062e5c343861ce4c11d99e20b668d173031
Serhii Haber <haberserhii@icloud.com>
2023-07-07 17:27:28 +0300

    Upd

--- autocheck.js
+++ autocheck.js
@@ -1,34 +1,19 @@
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return onSuccess(`Your order is accepted. Cooking pizza ${pizzaName}.`);
-    }
-    return onError(
-      `Error! There is no pizza with a name ${pizzaName} in the assortment.`
-    );
-  },
-};
-
-function makePizza(pizzaMessage) {
-  return pizzaMessage;
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  orderedItems.forEach(function (number, index) {
+    totalPrice += number;
+  });
+  return totalPrice;
 }
 
-function onOrderError(error) {
-  return error;
-}
-
-console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
-console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
-console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
-console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
 
 function f1() {
-  return (document.querySelector(".output-1").innerHTML = pizzaPalace.order(
-    "Smoked",
-    makePizza,
-    onOrderError
-  ));
+  return (document.querySelector(".output-1").innerHTML = calculateTotalPrice([
+    12, 85, 37, 4,
+  ]));
 }
 
 document.querySelector(".btn-1").onclick = f1;
