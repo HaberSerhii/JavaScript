858de9aafa55ef4a03716d428d1a42e8eed274f8
Serhii Haber <haberserhii@icloud.com>
2023-07-07 17:57:05 +0300

    upd

--- autocheck.js
+++ autocheck.js
@@ -1,19 +1,26 @@
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
-  orderedItems.forEach(function (number, index) {
-    totalPrice += number;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  numbers.forEach(function (number) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
   });
-  return totalPrice;
+
+  return filteredNumbers;
 }
 
-console.log(calculateTotalPrice([12, 85, 37, 4]));
-console.log(calculateTotalPrice([164, 48, 291]));
-console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
 
 function f1() {
-  return (document.querySelector(".output-1").innerHTML = calculateTotalPrice([
-    12, 85, 37, 4,
-  ]));
+  return (document.querySelector(".output-1").innerHTML = filterArray(
+    [12, 24, 8, 41, 76],
+    20
+  ));
 }
 
 document.querySelector(".btn-1").onclick = f1;
