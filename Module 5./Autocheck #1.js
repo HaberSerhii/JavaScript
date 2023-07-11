5ac4c017960d503a2731598dd283448d8a09f296
Serhii Haber <haberserhii@icloud.com>
2023-07-11 17:27:18 +0300

    upd

---
+++ Module 5./Autocheck #1.js
@@ -0,0 +1,23 @@
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+};
+
+console.log(pizzaPalace.order("Smoked"));
+
+function f1() {
+  document.querySelector(".output-1").innerHTML = pizzaPalace.order("Viennese");
+}
+
+document.querySelector(".btn-1").onclick = f1;
--- autocheck.js
+++ autocheck.js
@@ -1,13 +1,31 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter((user) => user.gender === gender)
-    .reduce((sum, user) => sum + user.balance, 0);
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  getOrdersLog() {
+    return this.orders
+      .map((order) => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map((order) => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter((order) => order.email === email);
+  },
 };
 
-console.log(getTotalBalanceByGender);
+console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
 
 function f1() {
-  document.querySelector(".output-1").innerHTML = getTotalBalanceByGender;
+  document.querySelector(".output-1").innerHTML =
+    historyService.getOrdersByEmail("artemis@coldmail.net");
 }
 
 document.querySelector(".btn-1").onclick = f1;
