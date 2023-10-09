817c0c277fb381084ea8a7015c9d0f9866ce4bde
Serhii Haber <haberserhii@icloud.com>
2023-10-09 17:14:01 +0300

    upd

--- autocheck.js
+++ autocheck.js
@@ -1,56 +1,9 @@
-class User {
-  email;
-
-  constructor(email) {
-    this.email = email;
+function repeatStr(n, s) {
+  let Str = "";
+  for (let i = 0; i < n; i += 1) {
+    Str += s;
   }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
+  return Str;
 }
 
-class Admin extends User {
-  blacklistedEmails = [];
-
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
-
-  constructor({ email, accessLevel }) {
-    super(email);
-    this.accessLevel = accessLevel;
-  }
-
-  blacklist(email) {
-    this.blacklistedEmails.push(email);
-  }
-
-  isBlacklisted(email) {
-    return this.blacklistedEmails.includes(email);
-  }
-}
-
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
-mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
-
-function f1() {
-  document.querySelector(".output-1").innerHTML =
-    mango.isBlacklisted("poly@mail.com");
-}
-
-document.querySelector(".btn-1").onclick = f1;
+console.log(repeatStr(3, "*"));
